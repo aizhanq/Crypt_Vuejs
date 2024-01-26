f20cbceee8833e52c8fce4adc535b4a230a05c83
aizhanq <aizhankarasartova@gmail.com>
2024-01-26 17:01:20 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import Input from './components/Input.vue'
+import Selector from './components/Selector.vue'
+
+export default {
+  components: {
+    Input,
+    Selector
+  }
+}
+</script>
+
+<template>
+<h1>CRYPTO</h1>
+<Input />
+<div className="selectors">
+  <Selector />
+  <Selector />
+</div>
+</template>
+
+<style scoped>
+.selectors {
+  display: flex;
+  justify-content: space-around;
+  width: 700px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <input type="number" placeholder="Enter the number">
+</template>
+
+<style scoped>
+input {
+    outline: none;
+    width: 500px;
+    position: relative;
+    top: -50px;
+    font-size: 1.2em;
+    border-radius: 3px;
+    border: 0;
+    padding: 10px 15px;
+    background: #fafafa;
+    color: #333;
+}
+</style>
